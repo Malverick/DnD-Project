c77e1e6c4ff0e6458b4a3c1c86103f1ccf5f3e71
Malverick <jamesfirth95@yahoo.co.uk>
2020-06-17 16:09:50 +0100

    GetAll finished

--- Back_End/Chars/src/main/java/com/DnD/Chars/Controller/ClassController.java
+++ Back_End/Chars/src/main/java/com/DnD/Chars/Controller/ClassController.java
@@ -1,8 +1,28 @@
 package com.DnD.Chars.Controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.DnD.Chars.Entity.ClassEntity;
+import com.DnD.Chars.Service.ClassService;
 
 @Controller
 public class ClassController {
 
+	private ClassService classService;
+	
+	private ClassController (@Autowired ClassService classService) {
+		this.classService = classService;
+	}
+	
+    @GetMapping(path = "/get/class/all")
+    public ResponseEntity<List<ClassEntity>> getAllClasses() {
+        return new ResponseEntity<>(classService.getAllClasses(), HttpStatus.OK);
+    }
+	
 }
--- Back_End/Chars/src/main/java/com/DnD/Chars/Service/ClassService.java
+++ Back_End/Chars/src/main/java/com/DnD/Chars/Service/ClassService.java
@@ -1,5 +1,24 @@
 package com.DnD.Chars.Service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.DnD.Chars.Entity.ClassEntity;
+import com.DnD.Chars.Repository.ClassRepository;
+
+@Service
 public class ClassService {
 
+	private ClassRepository classRepository;
+	
+	private ClassService(@Autowired ClassRepository classRepository) {
+		this.classRepository = classRepository;
+	}
+	
+	public List<ClassEntity> getAllClasses() {
+		List<ClassEntity> classes = (List<ClassEntity>) classRepository.findAll();
+		return classes;
+	}
 }
