797adcabf45f915e90b9674790bbf4b15800c5ed
Daniel Greenwood <dangreenwood@zoho.com>
2020-06-24 14:57:54 +0100

    create account form complete, need to link it to the backend

--- Front End/Javascript/accountCreate.js
+++ Front End/Javascript/accountCreate.js
@@ -16,18 +16,56 @@ function showPassword2() {
     }
 }
 
-$(".invalidEmail").hide();
+// Check to make sure that the Email is valid
+$("#invalidEmail").hide();
 $("#eMailAddr").change(function () {
     var input = $(this).val();
-    var comparitor = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/i;
+    var comparitor = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if (comparitor.test(input) == false) {
         $(this).css("color", "#c47171");
         console.log("false");
-        $(".invalidEmail").show();
+        $("#invalidEmail").show();
     } else {
         $(this).css("color", "ghostwhite");
         console.log("true");
-        $(".invalidEmail").hide();
-
+        $("#invalidEmail").hide();
     }
 }); 
+
+// Check to make sure that the password is strong enough 
+$("#invalidPass").hide();
+$("#password1").change(function () {
+    var input = $(this).val();
+    var comparitor = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/;
+    if (comparitor.test(input) == false) {
+        $(this).css("color", "#c47171");
+        console.log("false");
+        $("#invalidPass").show();
+
+    } else {
+        $(this).css("color", "ghostwhite");
+        console.log("true");
+        $("#invalidPass").hide();
+
+    }
+    passMatcher();
+});
+
+// Check to make sure password2 is the same as password1
+$("#password2").change(function () {
+    passMatcher();
+});
+
+$("#passMatcher").hide();
+function passMatcher() {
+    let a = $("#password2").val();
+    let b = $("#password1").val();
+    if (a == b) {
+        $("#passMatcher").hide();
+        console.log("hiding" + a + " - " + b);
+    } else {
+        $("#passMatcher").show();
+        console.log("showing" + a + " - " + b);
+    }
+}
+
