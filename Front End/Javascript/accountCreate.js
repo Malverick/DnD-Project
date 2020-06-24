b219e86f00c9bf425d5b64808147e649b7afe93c
Daniel Greenwood <dangreenwood@zoho.com>
2020-06-24 12:53:00 +0100

    added regex for email validation, changes text red upon invalid email address

--- Front End/Javascript/accountCreate.js
+++ Front End/Javascript/accountCreate.js
@@ -6,7 +6,8 @@ function showPassword1() {
     } else {
         x.type = "password";
     }
-} function showPassword2() {
+}
+function showPassword2() {
     var x = document.getElementById("password2");
     if (x.type === "password") {
         x.type = "text";
@@ -14,3 +15,16 @@ function showPassword1() {
         x.type = "password";
     }
 }
+
+$("#eMailAddr").change(function () {
+    var input = $(this).val();
+    var comparitor = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/i;
+    if (comparitor.test(input) == false) {
+        $(this).css("color", "#c47171");
+        console.log("false");
+    } else {
+        $(this).css("color", "ghostwhite");
+        console.log("true");
+
+    }
+}); 
